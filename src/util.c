36664f0fc80c96d4b369566ad6c436fcd2cbb693
yesil <egeyesil4@gmail.com>
2025-07-06 16:20:19 +0000

    Updated the read and write logic. Now getResponse doesnt block

--- src/util.c
+++ src/util.c
@@ -1,43 +1,43 @@
 #include "util.h"
 
 void sendResponse(int socket, char* str) {
-    char* strFinal = malloc(strlen(str) + 5);
-    if (!strFinal) {
-        fprintf(stderr, "Could not reallocate string to send as response\nError string: %s", strerror(errno));
-        return;
-    }
-    memcpy(strFinal, str, strlen(str));
-    strcpy(strFinal + strlen(str), "<END>\r");
-    write(socket, strFinal, strlen(strFinal));
+    write(socket, str, strlen(str));
 }
 
 char* getResponse(int socket) {
     size_t bufferSize = 1024;
-    char* buffer = malloc(sizeof(char) * bufferSize);
-    size_t bytes = -1;
+    char* buffer = malloc(bufferSize);
+    ssize_t bytes = -1;
     size_t responseSize = 0;
-    while (bytes > 0) {
+    while (1) {
+        bytes = recv(socket, buffer + responseSize, bufferSize, MSG_DONTWAIT);
+        if (bytes > 0) responseSize += bytes;
         if (responseSize > bufferSize) {
-            bufferSize = responseSize;
+            bufferSize *= 2;
             buffer = realloc(buffer, bufferSize);
+            if (buffer == NULL) {
+                fprintf(stderr, "Could not reallocate read buffer\nError string: %s\n", strerror(errno));
+                free(buffer);
+                return NULL;
+            }
         }
-        bytes = read(socket, buffer + (responseSize * sizeof(char)), bufferSize);
-        responseSize += bytes;
-        char* pos = strstr(buffer, "<END>\r");
-        if (pos != NULL) {
-            size_t realSize = pos - buffer;
-            char* o = malloc(realSize);
-            memcpy(o, buffer, realSize);
+
+        if (bytes == -1 && (errno == EAGAIN || errno == EWOULDBLOCK)) {
+            if (responseSize == 0) return NULL;
+            buffer = realloc(buffer, responseSize + 1);
+            if (buffer == NULL) {
+                fprintf(stderr, "Could not reallocate read buffer\nError string: %s\n", strerror(errno));
+                free(buffer);
+                return NULL;
+            }
+
+            buffer[responseSize] = '\0';
+            return buffer;
+        } else if (bytes == -1) {
+            fprintf(stderr, "Read function failed\nError string: %s\n", strerror(errno));
             free(buffer);
-            return o;
+            return NULL;
         }
     }
- 
-    if (bytes < 0) {
-        fprintf(stderr, "read function failed\nError string: %s\n", strerror(errno));
-        free(buffer);
-    }  
- 
-    return NULL;
 }
 
